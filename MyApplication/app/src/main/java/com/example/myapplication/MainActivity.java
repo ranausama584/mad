1cdd7018218391ecfdb6b5219840fc919028eff1
ranausama584 <68021880+ranausama584@users.noreply.github.com>
2020-07-08 21:48:33 +0500

    Task

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    // These are the global variables
+    EditText editName, editPassword;
+    TextView result;
+    Button buttonSubmit, buttonReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editName  = (EditText) findViewById(R.id.editName);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        result = (TextView) findViewById(R.id.tvResult);
+        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        /*
+            Submit Button
+        */
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = editName.getText().toString();
+                String password = editPassword.getText().toString();
+                result.setText("Name:\t" + name + "\nPassword:\t" + password );
+
+            }
+        });
+
+        /*
+            Reset Button
+        */
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editName.setText("");
+                editPassword.setText("");
+                result.setText("");
+                editName.requestFocus();
+            }
+        });
+    }
+}
